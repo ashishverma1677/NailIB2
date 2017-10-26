c34cf3961d7326c4a7954fac66e31dd86fffcead
ashishverma1677 <33121666+ashishverma1677@users.noreply.github.com>
2017-10-26 17:25:40 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,1 @@
-<?php
-
-$databaseHost = 'sg2plcpnl0056.prod.sin2.secureserver.net';
-$databaseName = 'nail_db';
-$databaseUsername = 'nail_user';
-$databasePassword = 'nail_pass';
-
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
- 
-?>
+]m,sdfhd
